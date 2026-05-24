053d476139d2f27c286ec010edd7bcbf4a48778a
vinit <vinitrabadiyasoft.en@gmail.com>
2026-05-24 18:36:24 +0300

    drawing  triangle on the canvas

--- script.js
+++ script.js
@@ -7,12 +7,28 @@ if (canvas.getContext) {
   console.error("Canvas not supported in this browser.");
 }
 
-function draw() {
-  ctx.fillStyle = "rgb(200 0 0)";
-  ctx.fillRect(10, 10, 50, 50);
+//triangle
+const angle = Math.PI / 2;
+const centerX = 40;
+const centerY = 80;
 
-  ctx.fillStyle = "rgb(0 0 200 / 50%)";
-  ctx.fillRect(30, 30, 50, 50);
-}
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-draw();
+ctx.save();
+ctx.translate(centerX, centerY);
+ctx.rotate(angle);
+
+ctx.beginPath();
+ctx.moveTo(0, -1);
+ctx.lineTo(-50, 30);
+ctx.lineTo(50, 30);
+ctx.closePath();
+
+ctx.fillStyle = "#ff0000";
+ctx.fill();
+
+ctx.lineWidth = 5;
+ctx.strokeStyle = "#000000";
+ctx.stroke();
+
+ctx.restore();
