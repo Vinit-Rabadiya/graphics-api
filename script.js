234834d682c4f166125c3dc256f42f22d07fb714
vinit <vinitrabadiyasoft.en@gmail.com>
2026-05-25 10:55:46 +0300

    ship movement controls using keyboard inputs

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
+const keys = {};
+window.addEventListener("keydown", function(e) { keys[e.code] = true; });
+window.addEventListener("keyup",   function(e) { keys[e.code] = false; });
+
 // ship position and angle
 const ship = {
   x: 400,
@@ -8,12 +12,21 @@ const ship = {
   angle: -Math.PI / 2  
 };
 
+//moving ship with keys
+function updateShip() {
+  if (keys["ArrowLeft"])  ship.angle -= 0.05;  
+  if (keys["ArrowRight"]) ship.angle += 0.05;   
+  if (keys["ArrowUp"]) {                        
+    ship.x += Math.cos(ship.angle) * 3;
+    ship.y += Math.sin(ship.angle) * 3;
+  }
+}
+
 //triangle
 function drawShip() {
   ctx.save();
-
-  ctx.translate(ship.x, ship.y);  
-  ctx.rotate(ship.angle);          
+  ctx.translate(ship.x, ship.y);
+  ctx.rotate(ship.angle);
 
   ctx.beginPath();
   ctx.moveTo(20, 0);     
@@ -25,10 +38,12 @@ function drawShip() {
   ctx.restore();
 }
 
+
 function gameLoop() {
   ctx.clearRect(0, 0, 800, 600);
+  updateShip();
   drawShip();
   requestAnimationFrame(gameLoop);
 }
 
-gameLoop();   
+gameLoop();
